c0591d22ee864e024c76d310ae56c8c26207a5ad
Neeraj Yadav <ny.neeraj12121@gmail.com>
2017-01-01 23:58:11 +0530

    first commit

---
+++ Comparring_ecommerce_product/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Comparring_ecommerce_product project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Comparring_ecommerce_product.settings")
+
+application = get_wsgi_application()
