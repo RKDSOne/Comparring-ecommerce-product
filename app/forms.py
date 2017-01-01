5e0658f6b06e70d73b276893c223149a0803c8f6
Neeraj Yadav <ny.neeraj12121@gmail.com>
2017-01-02 00:05:14 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+
+class search(forms.Form):
+    querry = forms.CharField(label='search_term',widget=forms.TextInput(attrs={'placeholder': 'Enter name of the product'})	)
