3ee406042ec945d1f18df163a03bb8acacc5daf3
Neeraj <ny.neeraj12121@gmail.com>
2017-01-02 22:57:54 +0530

    updated

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,4 @@
 from django import forms
 
-
 class search(forms.Form):
     querry = forms.CharField(label='search_term',widget=forms.TextInput(attrs={'placeholder': 'Enter name of the product'})	)
