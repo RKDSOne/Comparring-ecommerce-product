4be9391a06f56f1242f7829c136f43affdfa4d52
Neeraj <ny.neeraj12121@gmail.com>
2017-01-05 22:50:55 +0530

    API key updated

--- Comparring_ecommerce_product/settings.py
+++ Comparring_ecommerce_product/settings.py
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 import os
 
-=======
 """
 Django settings for Comparring_ecommerce_product project.
 
@@ -18,7 +16,6 @@ import os
 import pymysql
 
 pymysql.install_as_MySQLdb()
->>>>>>> 2550c8d65edfec042bdf330e7dc5db1f373473ff
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,15 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-<<<<<<< HEAD
-           }
-=======
-       # 'USER': 'root',
-       # 'HOST': '',
-       # 'PORT': '',
-       # 'PASSWORD': 'root'
     }
->>>>>>> 2550c8d65edfec042bdf330e7dc5db1f373473ff
 }
 
 
@@ -136,8 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
-=======
->>>>>>> 2550c8d65edfec042bdf330e7dc5db1f373473ff
